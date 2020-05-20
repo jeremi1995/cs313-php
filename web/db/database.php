25cd1aa9cf14e0c362af0de274708fcd45cff3ae
jeremi1995 <jeremibq@gmail.com>
2020-05-19 21:31:30 -0600

    display database/users

--- web/db/database.php
+++ web/db/database.php
@@ -7,7 +7,7 @@ $db = NULL;
 try {
 
 $dbUrl = 'postgres://uceotupgucrpqy:6ef80c902d24199baa647197a4f29d1efb097029ea827a18d5313dbb2c4a1ef3@ec2-52-207-25-133.compute-1.amazonaws.com:5432/d628mnu6q251l';
-//'postgres://uceotupgucrpqy:6ef80c902d24199baa647197a4f29d1efb097029ea827a18d5313dbb2c4a1ef3@ec2-52-207-25-133.compute-1.amazonaws.com:5432/d628mnu6q251l';
+
 
 $dbOpts = parse_url($dbUrl);
 $dbHost = $dbOpts["host"];
--- web/week05/week05.php
+++ web/week05/week05.php
@@ -5,6 +5,26 @@ $db = getDB();
 $statement = $db->prepare("SELECT * FROM users");
 //$statement->bindValue(':user_id', $user_id);
 
-$statement->execute();
+ while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    // The variable "row" now holds the complete record for that
+    // row, and we can access the different values based on their
+    // name
+    $personal_record_id = $row['id'];
+    $record_name = $row['user_name'];
+    $record_amount = $row['password'];
+    $record_date = $row['date_of_birth'];
+    echo "<tr>" . "<td>" . $record_name    . "</td>" .
+        "<td>" . $record_amount . "</td>" .
+        "<td>" . $record_date       .
+
+        "<form action='remove_pr.php' method='POST'>" .
+        "<button type='submit' class='btn btn-danger' name='remove'" .
+        "value='$personal_record_id'>Remove Entry</button></form>";
+
+
+    "</td>" .
+        "</tr>";
+}
+
 
 ?>
